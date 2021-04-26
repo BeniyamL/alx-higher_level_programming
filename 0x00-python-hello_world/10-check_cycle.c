d4f9403513081fd3afd78c9bf6e66768a261b56a
Beniyam Legesse <lbeniyam@gmail.com>
2021-04-26 04:48:20 -0700

    correction on cycle check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,18 +7,16 @@
  **/
 int check_cycle(listint_t *list)
 {
-	listint_t *fast, *slow;
+	listint_t *fast = list, *slow = list;
 
 	if (list == NULL || list->next == NULL)
 		return (0);
-	fast = list->next->next;
-	slow = list->next;
 	while(fast && slow && fast->next)
 	{
+		fast = fast->next->next;
+		slow = slow->next;
 		if (fast == slow)
 			return (1);
-		fast = fast ->next->next;
-		slow = slow->next;
 	}
 	return (0);
 }
