7c7d6ec496a212c9ce1bd2495e1a9e4941ba5c37
Beniyam Legesse <lbeniyam@gmail.com>
2021-04-27 05:02:43 -0700

    fix on task 8 to use only 2 print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     for c in str:
         asci = ord(c)
         if asci >= 97 and asci < 123:
-            print("{0:c}".format(asci - 32), end="")
-        else:
-            print(c, end="")
+            asci = asci - 32
+        print("{0:c}".format(asci), end="")
     print()
