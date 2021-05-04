cd198002a1de202d56aaf03af8987ebd27af0ef9
Beniyam Legesse <lbeniyam@gmail.com>
2021-05-04 06:55:30 -0700

    removal of 0 from print specifier

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,6 +9,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if j == len(matrix[i]) - 1:
-                print("{0:d}".format(matrix[i][j]))
+                print("{:d}".format(matrix[i][j]))
             else:
-                print("{0:d} ".format(matrix[i][j]), end="")
+                print("{:d} ".format(matrix[i][j]), end="")
