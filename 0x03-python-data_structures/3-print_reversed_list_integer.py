0ea862d283d84ade2f9a16d39946ce11e00a8756
Beniyam Legesse <lbeniyam@gmail.com>
2021-05-04 06:34:45 -0700

    removal of comments for fucntion

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    """
-    print_reversed_list_integer - print an integr list
-    @my_list: the given list
-    @Return : the reversed list
-    """
     if my_list is None:
         return
     for i in range(len(my_list) - 1, -1, -1):
