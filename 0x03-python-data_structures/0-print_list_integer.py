f683291ade4369024ec2fdf30558c3769a03661b
Beniyam Legesse <lbeniyam@gmail.com>
2021-05-04 05:56:50 -0700

    update on pep8 structure

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """
-    print_list_integer - print an integr list
-    @my_list: the given list
-    @Return : nothing
+    """print an integr list
+
+    Args:
+        my_list: the given list
+
+    Returns:
+        nothing
     """
     for i in range(len(my_list)):
         print("{0:d}".format(my_list[i]))
