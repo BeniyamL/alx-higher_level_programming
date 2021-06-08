c3f0af7ac867acb100277820c566228406b31c24
Beniyam Legesse <lbeniyam@gmail.com>
2021-06-08 11:45:51 -0700

    save to csv format

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Base class definition """
 import json
+import csv
 
 
 class Base:
@@ -96,5 +97,51 @@ class Base:
             with open(filename) as jsonfile:
                 list_dict = Base.from_json_string(jsonfile.read())
                 return [cls.create(**dictionary) for dictionary in list_dict]
+        except IOError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ function to save to csv
+
+        Arguments:
+            cls: the class
+            list_objs: list objects
+        Returns:
+            nothing
+        """
+        filename = cls.__name__ + ".csv"
+        if cls.__name__ == "Rectangle":
+            col_names = ["id", "width", "height", "x", "y"]
+        elif cls.__name__ == "Square":
+            col_names = ["id", "size", "x", "y"]
+        with open(filename, mode="w", newline="") as csvfile:
+            if list_objs is None or len(list_objs) == 0:
+                csvfile.write("[]")
+            else:
+                blankfile = csv.DictWriter(csvfile, fieldnames=col_names)
+                for obj in list_objs:
+                    blankfile.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ function to load csv files
+
+        Arguments:
+            cls: the class
+        Returns:
+            nothing
+        """
+        filename = cls.__name__ + ".csv"
+        if cls.__name__ == "Rectangle":
+            col_names = ["id", "width", "height", "x", "y"]
+        elif cls.__name__ == "Square":
+            col_names = ["id", "size", "x", "y"]
+        try:
+            with open(filename, mode="r", newline="") as csvfile:
+                l_dict = csv.DictReader(csvfile, fieldnames=col_names)
+                l_dict = [dict([key, int(value)] for key, value in d.items())
+                          for d in l_dict]
+                return [cls.create(**obj) for obj in l_dict]
         except IOError:
             return []
