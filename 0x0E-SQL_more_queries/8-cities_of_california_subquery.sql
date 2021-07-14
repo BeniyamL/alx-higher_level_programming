2d64dbc3ff2744c9f41fb92ab4595e06a178afac
Beniyam Legesse <lbeniyam@gmail.com>
2021-07-14 00:18:21 -0700

    correction on task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- sql script to list all the cities of california
 
-SELECT `c`.`id`, `c`.`name` from `cities` c
+SELECT `c`.`id`, `c`.`name` FROM `cities` c
 WHERE `c`.`state_id` = 
 		(SELECT `s`.`id` FROM `states` s
 		WHERE `s`.`name` = 'California');
