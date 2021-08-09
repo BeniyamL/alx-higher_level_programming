1e6f1ed0f6ec4f8144ba4051f0a6fcb332040334
Beniyam Legesse <lbeniyam@gmail.com>
2021-08-08 23:05:29 -0700

    module documentation of 1-filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
                            passwd=sys.argv[2], db=sys.argv[3])
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states
+    cur.execute("SELECT * FROM states\
                 WHERE name like 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
